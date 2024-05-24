3562ada5e5b33d754ea187baf9e3a73a341c9ff5
Gustavo Henrique <48604457+Tavinhuu@users.noreply.github.com>
2024-05-24 09:53:59 -0300

    Update personagem_atributos.h

--- game/personagem_atributos.h
+++ game/personagem_atributos.h
@@ -18,4 +18,19 @@ typedef struct //estrura para colocar as informaçoes do jogador e também de in
 
 }Personagem_atributos;
 
+
+
+// inventario do jogador, usei numeros para verificar se ele tem ou não, exemplo o primeiro pergaminho é 1, então quando for para usar
+// so precisa verificar se o pergaminho que ele tem é o 1, 2,3 e etc, mesma coisa com outros itens. No caso de espadas é melhor aumentar direto
+// o ataque do personagem dele
+typedef struct 
+{
+    int pergaminho;
+
+}Personagem_inventario;
+
+
+
+
+
 #endif // MINHA_STRUCT_H
