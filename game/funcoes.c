4f86ce22d75ab20937b952869591e1839397d260
Gabriel Cardoso <89615327+gabrielentediado@users.noreply.github.com>
2024-06-06 22:33:31 -0300

    update

--- game/funcoes.c
+++ game/funcoes.c
@@ -144,3 +144,123 @@ int cadastro(){
     
 
 }
+
+// quarto
+
+int quarto(){
+
+    
+        if(verificador == 0){ // para entrar no quarto ele precisa entrar na sala primeiro
+
+            puts("Você decide entrar no quarto, entretanto a porta esta trancada e não há formas de entrar\n\n");
+            explorarCasa();
+
+        } else {
+
+            printf("O quarto esta escuro e abandonado, há restos de roupas pelo chão...\n");
+            sleep(1);
+            printf("Parece ter sido vasculhado por alguem recentemente...\n");
+            sleep(1);
+            puts("Em meio a bagunça, você observa alguns rasgos na parede. \n");
+            sleep(1);
+            puts("Na parde ao lado uma escrita em sangue feito a mão 'eles são reais' \n\n");
+            sleep(1);
+            puts("Logo abaixo um báu de madeira, você deseja abrir? \n");
+            
+            puts("1-para abrir\n2-para voltar");
+
+            puts("\n\n*Você contará com sua sorte para conseguir ou não abrir o baú*"); //começar a usar o dado daqui depois da introdução
+
+            scanf("%d", &menuNav);
+
+            int dadoGerado = dado();
+
+
+            if (menuNav == 1){
+
+                // dado gerado
+
+
+                Personagem_inventario inv;
+                
+
+                if(dadoGerado >= 4){
+                    
+                    sleep(1);
+                    printf("valor da jogadada do dado : %d \n", dadoGerado); // mostra o valor do dado
+                    
+                    printf("Você empurra o baú, uma parte se solta e você pode conferir o que há dentro\n\n");
+                    printf("Legal, você encontrou um pergaminho, item adicionado ao seu inventário\n");
+
+                    inv.pergaminho = 1;
+
+                    sleep(2);
+                    printf("Acho que não há mais o que explorar, vamos sair da casa!");
+                    opcoes();
+                    
+                } else {
+
+                    sleep(1);
+                    printf("valor da jogadada do dado : %d \n", dadoGerado); // mostra o valor do dado
+
+                    printf("O baú está muito bem lacrado, infelizmente você não consegue abrir\n");
+                    sleep(2);
+                    printf("Acho que não há mais o que explorar, vamos sair da casa!");
+                    opcoes();
+                }
+
+            } else {
+
+            }
+
+
+        }
+        break;
+
+}
+
+//sala
+
+void sala(){
+
+    
+        if(verificador == 0){ // verificar se ele ja entrou ou nao na sala
+            printf("Você entra silenciosamente na sala principal.\n");
+            sleep(1);
+
+            //removi as funções sprites do código, falta ainda implementar os headers
+
+            sleep(1);
+            printf("O local está horrível, há pedaços de madeira podre em toda parte\n");
+            sleep(1);
+            printf("É possível ver algo brilhante refletindo em uma pequena mesa de centro\n");
+            sleep(1);
+            puts("Deseja verificar o que é?\n");
+            printf("1 - Sim\n2 - Não\n");
+            scanf("%d", &menuNav);
+            
+            if (menuNav == 1){
+                puts("Você chega perto para ver, afasta um pouco os pedaçoes destroçados de madeira...\n");
+                sleep(1);
+                puts("É isso! Uma chave, com certeza é a chave que precisa para entrar no quarto\n");
+
+                verificador = 1; // agora ele ja pode andar e entrar no quarto
+
+                sleep(1);
+                printf("Vamos voltar e explorar o quarto...\n");
+                explorarCasa();
+
+            }else{
+
+                puts("Ah, melhor não ver o que é, devemos voltar!");
+                sleep(1);
+                opcoes();
+
+            }
+            
+        }
+
+
+
+}
+    
--- game/main.c
+++ game/main.c
@@ -50,6 +50,16 @@ void andar(){
     } else {
         printf("continuar daqui.");
 
+        printf("Jogue o dado para dar um passo adiante \n");
+        printf("aparte 1 para jogar o dado ");
+        scanf("%d", menuNav);
+
+        switch(menuNav){
+            case 1:
+                
+
+        }
+
         //inciar um evento aleatorio:
     }
 }                 
@@ -71,115 +81,20 @@ void explorarCasa(){
 
     //quarto
     case 1:
-        if(verificador == 0){ // para entrar no quarto ele precisa entrar na sala primeiro
+    //quarto
+    quarto();
 
-            puts("Você decide entrar no quarto, entretanto a porta esta trancada e não há formas de entrar\n\n");
-            explorarCasa();
-
-        } else {
-
-            printf("O quarto esta escuro e abandonado, há restos de roupas pelo chão...\n");
-            sleep(1);
-            printf("Parece ter sido vasculhado por alguem recentemente...\n");
-            sleep(1);
-            puts("Em meio a bagunça, você observa alguns rasgos na parede. \n");
-            sleep(1);
-            puts("Na parde ao lado uma escrita em sangue feito a mão 'eles são reais' \n\n");
-            sleep(1);
-            puts("Logo abaixo um báu de madeira, você deseja abrir? \n");
-            
-            puts("1-para abrir\n2-para voltar");
-
-            puts("\n\n*Você contará com sua sorte para conseguir ou não abrir o baú*"); //começar a usar o dado daqui depois da introdução
-
-            scanf("%d", &menuNav);
-
-            int dadoGerado = dado();
-
-
-            if (menuNav == 1){
-
-                // dado gerado
-
-
-                Personagem_inventario inv;
-                
-
-                if(dadoGerado >= 4){
-                    
-                    sleep(1);
-                    printf("valor da jogadada do dado : %d \n", dadoGerado); // mostra o valor do dado
-                    
-                    printf("Você empurra o baú, uma parte se solta e você pode conferir o que há dentro\n\n");
-                    printf("Legal, você encontrou um pergaminho, item adicionado ao seu inventário\n");
-
-                    inv.pergaminho = 1;
-
-                    sleep(2);
-                    printf("Acho que não há mais o que explorar, vamos sair da casa!");
-                    opcoes();
-                    
-                } else {
-
-                    sleep(1);
-                    printf("valor da jogadada do dado : %d \n", dadoGerado); // mostra o valor do dado
-
-                    printf("O baú está muito bem lacrado, infelizmente você não consegue abrir\n");
-                    sleep(2);
-                    printf("Acho que não há mais o que explorar, vamos sair da casa!");
-                    opcoes();
-                }
-
-            } else {
-
-            }
-
-
-        }
-        break;
-    //sala principal
     case 2:
-        if(verificador == 0){ // verificar se ele ja entrou ou nao na sala
-            printf("Você entra silenciosamente na sala principal.\n");
-            sleep(1);
+    //sala
+    sala();
 
-            //removi as funções sprites do código, falta ainda implementar os headers
-
-            sleep(1);
-            printf("O local está horrível, há pedaços de madeira podre em toda parte\n");
-            sleep(1);
-            printf("É possível ver algo brilhante refletindo em uma pequena mesa de centro\n");
-            sleep(1);
-            puts("Deseja verificar o que é?\n");
-            printf("1 - Sim\n2 - Não\n");
-            scanf("%d", &menuNav);
-            if (menuNav == 1){
-                puts("Você chega perto para ver, afasta um pouco os pedaçoes destroçados de madeira...\n");
-                sleep(1);
-                puts("É isso! Uma chave, com certeza é a chave que precisa para entrar no quarto\n");
-
-                verificador = 1; // agora ele ja pode andar e entrar no quarto
-
-                sleep(1);
-                printf("Vamos voltar e explorar o quarto...\n");
-                explorarCasa();
-
-            }else{
-
-                puts("Ah, melhor não ver o que é, devemos voltar!");
-                sleep(1);
-                opcoes();
-
-            }
-            
-        }
     default:
         
-        //para não parar a execução do jogo do nada
-        sleep(1);
-        explorarCasa();
-    }
+    //para não parar a execução do jogo do nada
+    sleep(1);
+    explorarCasa();
 
+    }
 
 }
 //main -> cadastro -> mundo
@@ -234,7 +149,7 @@ int  opcoes()
 // |_| |_| |_|\__,_|_|_| |_|
           
 int main(){
-
+    setlocale (LC_ALL, "Portuguese");
     int lingua;
 
     printf("%s", titulo);
@@ -257,12 +172,11 @@ int main(){
     switch (lingua)
     {
     case 1:
-        setlocale (LC_ALL, "Portuguese");
         cadastro();
         break;
     case 2:
         //para testes
-        setlocale (LC_ALL, "Portuguese");
+        
         strcpy(personagem_principal.classe, "Humano");
         personagem_principal.vida==999;
         personagem_principal.ataque==999;
